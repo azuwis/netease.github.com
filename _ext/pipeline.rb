b48bf98b6440d7b60bd16eb05ff622cc61c4083a
Zhong Jianxin <azuwis@gmail.com>
2014-04-17 09:41:36 +0800

    Add news handling, replace base layout

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,8 +1,11 @@
 require 'bootstrap-sass'
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new '/news'
-  # extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Posts.new '/news'
+  extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+
+  helper Awestruct::Extensions::Partial
+  helper Awestruct::Extensions::Relative
 end
